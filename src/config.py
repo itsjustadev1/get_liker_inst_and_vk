89d4563fcdf2bffb184890647dce38d952d78d11
itsjustadev <86054371+itsjustadev@users.noreply.github.com>
2023-03-27 17:49:37 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-userName = 'clean.ann.dry'
-password = 'hesoyam11'
-TOKEN = '6125163117:AAENHIAT0MiDrvFcZbwSE8QuZwS14zLF-JI'
+userName = ''
+password = ''
+TOKEN = ''
 hashtagInst = 'тыващекрутой'
 hashtagVk = 'глянь'
-access_token = "vk1.a.vRM7HMXGapfHOgRwfjDfN1URr6mwF-OTVZgIXfqGmRmJWmZ4oPP55ZUS7ieGg6D0YJ48uRhdjYo8CpSWe3kRIIaGkYPUNcLuj3M2Yp8hPmpZdd-FJdl0KAoouzue-ViwVF6D5Er3tJXraEVGi300ZqQSe12YNwucELnqghqs-_5SFib5N0NprOvhhw9pnWRkFoF3cnBWADNbzaAzPQ009Q"
+access_token = ''
